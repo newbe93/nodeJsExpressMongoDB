e486a309bee26ae5c5c427efa17976b8ccc019e0
dongsu <tlsehd930@gmail.com>
2023-03-23 21:16:40 +0900

    router 미들웨어 수정

--- routes/shop.js
+++ routes/shop.js
@@ -19,11 +19,11 @@ function isLogin(req, res, next) {
   }
 }
 router.use(isLogin);
-router.get("/shirts", isLogin, function (req, res) {
+router.get("/shirts", function (req, res) {
   res.send("셔츠 파는 페이지입니다.");
 });
 
-router.get("/pants", isLogin, function (req, res) {
+router.get("/pants", function (req, res) {
   res.send("바지 파는 페이지입니다.");
 });
 
